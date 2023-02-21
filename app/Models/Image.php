ab17ac78736a2a696cd34df608f8b8e46f0097b9
Lelkes István <47304380+ilelkes@users.noreply.github.com>
2023-02-21 22:05:42 +0100

    Initial commit

---
+++ app/Models/Image.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Image extends Model
+{
+    use HasFactory;
+
+    public static function makeDirectory()
+    {
+        $subFolder = 'images/' . date('Y/m/d');
+        Storage::makeDirectory($subFolder);
+        return $subFolder;
+    }
+
+    public static function getDimension($image)
+    {
+        [$width, $height] = getimagesize(Storage::path($image));
+        return $width . "x" . $height;
+    }
+
+    public function scopePublished($query)
+    {
+        return $query->where('is_published', true);
+    }
+
+    public function fileUrl()
+    {
+        return Storage::url($this->file);
+    }
+}
